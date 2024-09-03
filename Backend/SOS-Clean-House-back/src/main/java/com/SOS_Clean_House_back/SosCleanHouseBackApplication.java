3d5a75c1bb80a9d58b17ab40bf44740c3647e2ab
Jaíne Rosa <jaine.rosa_11@live.com>
2024-09-03 19:30:56 -0300

    feat (main): Criando projeto backend

---
+++ Backend/SOS-Clean-House-back/src/main/java/com/SOS_Clean_House_back/SosCleanHouseBackApplication.java
@@ -0,0 +1,13 @@
+package com.SOS_Clean_House_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosCleanHouseBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosCleanHouseBackApplication.class, args);
+	}
+
+}
---
+++ Backend/SOS-Clean-House-back/src/test/java/com/SOS_Clean_House_back/SosCleanHouseBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SOS_Clean_House_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosCleanHouseBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
