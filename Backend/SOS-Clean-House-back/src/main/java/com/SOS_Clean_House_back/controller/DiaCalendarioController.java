09771ef92e1810a82185574d5c2b43ae12736796
Jaíne Rosa <jaine.rosa_11@live.com>
2024-09-10 20:41:15 -0300

    feat (DiaCalendario) Alteração de nome da classe

--- Backend/SOS-Clean-House-back/src/main/java/com/SOS_Clean_House_back/controller/DiaCalendarioController.java
+++ Backend/SOS-Clean-House-back/src/main/java/com/SOS_Clean_House_back/controller/DiaCalendarioController.java
@@ -1,7 +1,7 @@
 package com.SOS_Clean_House_back.controller;
 
 import com.SOS_Clean_House_back.model.DiaCalendario;
-import com.SOS_Clean_House_back.service.CalendarioService;
+import com.SOS_Clean_House_back.service.DiaCalendarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,35 +10,35 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/calendario")
-public class CalendarioController {
+public class DiaCalendarioController {
 
     @Autowired
-    CalendarioService calendarioService;
+    DiaCalendarioService diaCalendarioService;
 
     @GetMapping
     public ResponseEntity<List<DiaCalendario>> findAll(){
-        return ResponseEntity.ok(calendarioService.findAll());
+        return ResponseEntity.ok(diaCalendarioService.findAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<DiaCalendario> findById(@PathVariable Integer id){
-        return  ResponseEntity.ok(calendarioService.findById(id));
+        return  ResponseEntity.ok(diaCalendarioService.findById(id));
     }
 
     @PostMapping
     public ResponseEntity<DiaCalendario> create(@RequestBody DiaCalendario diaCalendario){
-        return ResponseEntity.ok(calendarioService.save(diaCalendario));
+        return ResponseEntity.ok(diaCalendarioService.save(diaCalendario));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<DiaCalendario> update(@RequestBody DiaCalendario diaCalendario){
-        return ResponseEntity.ok(calendarioService.save(diaCalendario));
+        return ResponseEntity.ok(diaCalendarioService.save(diaCalendario));
     }
 
     @DeleteMapping("/{id}")
     public  ResponseEntity<DiaCalendario> delete(@PathVariable Integer id){
-        DiaCalendario diaCalendario = calendarioService.findById(id);
-        calendarioService.delete(diaCalendario);
+        DiaCalendario diaCalendario = diaCalendarioService.findById(id);
+        diaCalendarioService.delete(diaCalendario);
         return ResponseEntity.ok(diaCalendario);
     }
     
--- Backend/SOS-Clean-House-back/src/main/java/com/SOS_Clean_House_back/model/DiaCalendario.java
+++ Backend/SOS-Clean-House-back/src/main/java/com/SOS_Clean_House_back/model/DiaCalendario.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @AllArgsConstructor @NoArgsConstructor @Data @Builder
-public class Calendario {
+public class DiaCalendario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
